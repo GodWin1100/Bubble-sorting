9222a7e231d4c826499de02fcbccb6739f332077
l._shivam_.l <44332286+GodWin1100@users.noreply.github.com>
2019-04-26 11:32:41 +0530

    Update main.c

--- main.c
+++ main.c
@@ -4,17 +4,21 @@
 int main()
 {
     int i,j,temp,n,m,a[10];
+    //Entering number of elements in array
     printf("\nEnter the number of element of array");
     scanf("%d",&n);
+    //Entering elements in array
     printf("\nEnter elements in array");
     for(i=0;i<n;i++)
         scanf("%d",&a[i]);
-        printf("main array");
+    //displaying elements in array
+    printf("main array");
         for(j=0;j<n;j++)
         {
         printf("\t%d",a[j]);
         }
-        printf("\n");
+    printf("\n");
+    //sorting elements in array step by step and displaying every step
     for(i=0;i<n-1;i++)
     {
         for(j=0;j<n-1;j++)
@@ -25,18 +29,18 @@ int main()
                 a[j]=a[j+1];
                 a[j+1]=temp;
             }
-            printf("changing of j");
-            for(m=0;m<n;m++)
-        {
-        printf("\t%d",a[m]);
-        }
+        printf("changing of j");
+        for(m=0;m<n;m++)
+            {
+            printf("\t%d",a[m]);
+            }
         printf("\n");
         }
-        for(j=0;j<n;j++)
+    for(j=0;j<n;j++)
         {
         printf("\t%d",a[j]);
         }
-        printf("\n");
+    printf("\n");
     }
     printf("\nLargest %d",a[n-1]);
     printf("\n second %d",a[n-2]);
